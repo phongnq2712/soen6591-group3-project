7c2e7b5c30918013c1459a7f89078fc07967f6af
phongnq <phongnq2712@gmail.com>
2023-04-18 15:11:07 -0400

    updated code churns 4th

--- soen6591-group3-project/src/main/java/com/concordia/soen/process/metrics/TotalNumberOfCodeChurns.java
+++ soen6591-group3-project/src/main/java/com/concordia/soen/process/metrics/TotalNumberOfCodeChurns.java
@@ -3,50 +3,57 @@ package com.concordia.soen.process.metrics;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.DepthWalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.diff.*;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 
 public class TotalNumberOfCodeChurns {
 	private static Git git;
 
-	private static int calculateChurn(Git git, RevCommit commit, RevCommit parentCommit) throws IOException {
-        // Set up the diff formatter
-        DiffFormatter diffFormatter = new DiffFormatter(System.out);
-        diffFormatter.setRepository(git.getRepository());
-        diffFormatter.setDiffComparator(RawTextComparator.DEFAULT);
-        diffFormatter.setDetectRenames(true);
-        
-        // Get the diffs
-        List<DiffEntry> diffs = diffFormatter.scan(parentCommit, commit);
-        // Calculate the churn
-        int churn = 0;
-        int addedLines = 0;
-        int deletedLines = 0;
-        int modifiedLines = 0;
-        for (DiffEntry diff : diffs) {
-            if (diff.getChangeType() == DiffEntry.ChangeType.ADD) {
-            	addedLines += diff.getNewPath().length();
-//                System.out.println("added: " + diff.getNewPath().length());
-            } else if (diff.getChangeType() == DiffEntry.ChangeType.DELETE) {
-            	deletedLines += diff.getOldPath().length();
-//                System.out.println("deleted: " + diff.getOldPath().length());
-            } else if (diff.getChangeType() == DiffEntry.ChangeType.MODIFY) {
-            	modifiedLines += diff.getNewPath().length() - diff.getOldPath().length();
-//                System.out.println("modified: " + (diff.getNewPath().length() - diff.getOldPath().length()));
+	public static int getCodeChurn(Repository repository, RevCommit commit) throws IOException {
+        int codeChurn = 0;
+
+        try (RevWalk revWalk = new RevWalk(repository, 100)) {
+            RevTree parentTree = commit.getParentCount() > 0 ? revWalk.parseTree(commit.getParent(0).getId()) : null;
+            RevTree commitTree = revWalk.parseTree(commit.getId());
+
+            if (parentTree != null) {
+                try (ObjectReader reader = repository.newObjectReader();
+                     DiffFormatter diffFormatter = new DiffFormatter(System.out)) {
+                    diffFormatter.setRepository(repository);
+                    List<DiffEntry> diffs = diffFormatter.scan(parentTree, commitTree);
+                    for (DiffEntry diff : diffs) {
+                        // Only consider modifications to Java files
+                        if (diff.getNewPath().endsWith(".java")) {
+                            List<Edit> edits = diffFormatter.toFileHeader(diff).toEditList();
+                            for (Edit edit : edits) {
+                            	codeChurn += edit.getLengthA(); // deleted lines
+                                codeChurn += edit.getLengthB(); // added lines
+                            }
+                        }
+                    }
+                }
+            } else {
+                // If there are no parents (i.e., first commit), consider all lines as code churn
+                codeChurn = countLines(commit.getFullMessage());
             }
         }
-        churn = addedLines + deletedLines + modifiedLines;
-        
-        return churn;
+
+        return codeChurn;
     }
-	  
+
+    public static int countLines(String text) {
+        String[] lines = text.split("\r\n|\r|\n");
+        return lines.length;
+    }
 	  
 	public static void main(String[] args) {
 		try {
@@ -56,24 +63,17 @@ public class TotalNumberOfCodeChurns {
 		    git = new Git(repo);
 			Iterable<RevCommit> commits = git.log().all().call();
 			int totalChurn = 0;
+			int codeChurn = 0;
             for (RevCommit commit : commits) {
-            	 String commitId = commit.getName();
-                 String commitMessage = commit.getFullMessage();
-                 System.out.println(commitId);
-                 System.out.println(commitMessage);
-                 
-                 // Get the parent commit
-                 RevCommit parentCommit = null;
-                 if (commit.getParentCount() > 0) {
-                     parentCommit = commit.getParent(0);
-                 }
-                 
-                 // Calculate the churn for this commit
-                 int churn = calculateChurn(git, commit, parentCommit);
-                 totalChurn += churn;
-                 System.out.println("Total of code churns in this commit: " + churn);
+            	System.out.println("Commit ID: " + commit.getId().getName());
+                System.out.println("Date: " + commit.getAuthorIdent().getWhen());
+                System.out.println("Message: " + commit.getFullMessage());
+                codeChurn = getCodeChurn(repo, commit);
+                System.out.println("Code churn: " + codeChurn);
+                System.out.println("---------------------------------------------");
+                totalChurn += codeChurn;
             }
-            System.out.println("--------------------------------------------------");
+            System.out.println("********************************************");
             System.out.println("Total of code churns in branch: " + totalChurn);
 		} catch (InvalidRemoteException e) {
 			e.printStackTrace();
